e61404ccc88c73bf787c177b6f8e92bbe718df10
chuch <chuchoislas01@hotmail.com>
2023-09-02 21:27:52 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiConWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double PI = 0;
+            double i = 1;
+            while (Math.Abs((Math.PI - PI)) > .01) 
+                {
+                if(i % 2==0)
+                {
+                    PI = PI - (4 / ((i * 2) - 1));
+                }
+                else
+                {
+                    PI = PI + (4 / ((i * 2) - 1));
+                }
+                Console.WriteLine(PI);
+                i++;
+                }
+            Console.WriteLine("El valor de pi con la iteracion {0} es {1}",i,PI);
+            Console.ReadLine();
+        }
+    }
+}
